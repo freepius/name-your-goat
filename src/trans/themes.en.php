e8522e997fda4b600bd3959c7a917bbf541dec16
freepius <contact@freepius.net>
2020-09-18 21:15:34 +0200

    Add 2 new godparents

--- src/trans/themes.en.php
+++ src/trans/themes.en.php
@@ -6,4 +6,7 @@ return [
     "Vins et cépages" => "Wines or grape varieties",
     "Noms en «ette»" => "Names ending with \"ette\"",
     "Australie" => "Australia",
+    "Alsace" => "Alsace (french territory)",
+    "Surnoms d'enfants" => "Children nicknames",
+    "Plantes aromatiques" => "Aromatic plants",
 ];
